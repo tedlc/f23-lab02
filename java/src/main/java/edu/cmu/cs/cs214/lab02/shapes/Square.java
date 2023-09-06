fd9916b3c40d9f19e9c778619da1eb7fd701a0c6
Zhiyu Yuan <zhiyuyuan@zhiyus-air.mynetworksettings.com>
2023-09-06 01:32:23 -0400

    Trying to create an interface

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Square.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Square.java
@@ -3,7 +3,7 @@ package edu.cmu.cs.cs214.lab02.shapes;
 public class Square implements Shape {
     public double sideLen;
 
-    public Square(double sideLen) {
+    private Square(double sideLen) {
         this.sideLen = sideLen;
     }
     
